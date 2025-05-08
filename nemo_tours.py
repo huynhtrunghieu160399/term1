d107f50b83a0f311d1d8cef19fa4f2bc87820512
Trung Hieu HUYNH <12281833@cqumail.com>
2025-05-08 11:44:19 +1000

    Author: HenryH Detail: Add 04 sub_function to support function_1

--- nemo_tours.py
+++ nemo_tours.py
@@ -1,18 +1,18 @@
-#----------------------------------------------------------------------------------------------------------------------
-# TODO -- create header comments
+#*********************************************************************************************************************
 # Name: Trung Hieu HUYNH
 # ID: 12281833
 # Project name: Nemo Reef Tours Program
 # 
-#----------------------------------------------------------------------------------------------------------------------
-# Cau hoi:
+#*********************************************************************************************************************
+# Question:
 # 1. Cac bien nen duoc khai bao theo dinh dang nao: camel hay ...
-# 2. 
-#----------------------------------------------------------------------------------------------------------------------
+# 2.
+# 3. 
+#*********************************************************************************************************************
 
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # Constants Declaration
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 ENTER_BOOKING = 1
 DISPLAY_BOOKINGS = 2
 DISPLAY_STATISTICS = 3
@@ -21,22 +21,22 @@ SAVE_BOOKINGS = 5
 READ_BOOKINGS = 6
 EXIT = 7
 
-# TODO -- declare any further constants
+# Declare any further constants
 PASSENGER_CHARGE = 95.50
 TEN_PERCENT_DISCOUNT = 10
 FIFTEEN_PERCENT_DISCOUNT = 15
 TWENTY_PERCENT_DISCOUNT = 20
-#------------------------------------------------------------------------------
-# TODO -- declare two lists, one for the booking names and another for the number of passengers
+#-----------------------------------------------------------------------------
+# Declare two lists, one for the booking names and another for the number of passengers
 booking_names = []
 booking_passengers = []
 
-# TODO -- declare variable for the number of bookings entered (integer)
+# Declare variable for the number of bookings entered (integer)
 number_of_bookings = 0
 
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # Function Definition
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 def print_heading():
     print("{:30s}{:11s}{:6s}".format("Booking name","Passengers", "Charge"))
 
@@ -75,55 +75,88 @@ def process_menu_item():
             read_bookings()
         choice = get_menu_item()
 
-# Function 1
-def enter_booking():
-    
-    # Step 1: 
-    # Read in the booking name (as a string)
-    booking_name =""
-    booking_name = str(input("Please enter the booking name ==> "))
-    
-    # Validation loop
+#--------------------------------------
+# Sub_function 1:
+#--------------------------------------
+def get_booking_name():
+    booking_name = input("Please enter the booking name ==> ")
     while booking_name == "":
-        if booking_name != "":
-            break
-        else:
-            print("ERROR: booking name cannot be blank.")
-            booking_name = str(input("Please enter the booking name ==> "))
+        print("ERROR: booking name cannot be blank.")
+        booking_name = input("Please enter the booking name ==> ")
+    return booking_name
 
-    number_of_passengers = ""
-    
-    # Step 2: 
-    # Validation loop
+
+#--------------------------------------
+# Sub_function 2:
+#--------------------------------------
+def get_number_of_passenger(booking_name):
     while True:
-        # Read in the number of passengers
-        # Hint: "Enter the number of passengers for " + booking_name + " ==> "
-        number_of_passengers = input("Please enter the number of passengers for " + "'" + booking_name + "'"+ " ==> ")
-
-        if number_of_passengers != "":
-            all_digits = True
-            for ch in number_of_passengers:
-                if ch < "0" or ch > "9":
-                    all_digits = False
+        num_str = input("Enter the number of passengers for '" + booking_name + "' ==> ")
+        if num_str != "":
+            is_valid = True
+            for ch in num_str:
+                if ch < '0' or ch > '9':
+                    is_valid = False
                     break
+                
+            if is_valid:
+                num = int(num_str)
+                if num >= 1:
+                    return num
+        print("ERROR: must be numeric and number of passengers must be greater than or equal to one")
 
-            if all_digits:
-                number = int(number_of_passengers)
-                if number >= 1:
-                    break
-                else:
-                    print("ERROR: must be numeric and number of passengers must be grater than or equal to one")
-    # Step 3
-	# TODO -- add the booking name and the number of passengers to the parallel lists
+
+#--------------------------------------
+#Sub_function 3:
+#--------------------------------------
+def calculate_charge(passengers):
+    charge = passengers * PASSENGER_CHARGE
+    if passengers >= 11:
+        charge *= (1 - TWENTY_PERCENT_DISCOUNT / 100)
+    elif passengers >= 6:
+        charge *= (1 - FIFTEEN_PERCENT_DISCOUNT / 100)
+    elif passengers >= 3:
+        charge *= (1 - TEN_PERCENT_DISCOUNT / 100)
+    return charge
+
+
+#--------------------------------------
+#Sub_function 4: Print receipt
+#--------------------------------------
+def print_receipt(name, passengers, charge):
+    print("\n---Nemo Tours Receipt---")
+    print("Booking name: ",name)
+    print("Number of passenger: ",passengers)
+    print("Total charge: " + "$" + format(charge, ".2f"))
+
+#----------------------------------------------------------
+# Function 1:
+#----------------------------------------------------------
+def enter_booking():
+    global number_of_bookings
+
+    # Calling "Sub_function 1" - get_booking_name: to read in the booking name (as a string)
+    booking_name = get_booking_name()
     
-    
-	# TODO -- display the booking name, number of passengers and the charge as a receipt as per the specification
+    # Calling "Sub_function 2" - get_number_of_passenger: to read in the number of passengers
+    passengers = get_number_of_passenger(booking_name)
 
-    # TODO -- increment the number of bookings variable
+    # Calling "Sub_function 3" - calculate_charge: to calculate the charge
+    charge = calculate_charge(passengers)
 
-    pass
+    # Add the booking name and the number of passengers to the parallel lists
+    booking_names.append(booking_name)
+    booking_passengers.append(passengers)
 
-# Function 2
+    # Increment the number of bookings variable
+    number_of_bookings += 1
+
+    # Calling "Sub_function 4" -  print_receipt: to print the receipt
+    print_receipt(booking_name, passengers, charge)
+
+#----------------------------------------------------------
+# Function 2:
+#----------------------------------------------------------
 def display_bookings():
     print_heading()
 
@@ -133,7 +166,9 @@ def display_bookings():
 
     pass
 
+#----------------------------------------------------------
 # Function 3
+#----------------------------------------------------------
 def display_statistics():    
     # TODO -- check if there has been an booking entered (do this after getting the other functionality working)
 
@@ -141,7 +176,9 @@ def display_statistics():
     
     pass
 
+#----------------------------------------------------------
 # Function 4
+#----------------------------------------------------------
 def search_bookings():
     # TODO -- check if there has been an booking entered (do this after getting the other functionality working)
 
@@ -153,12 +190,16 @@ def search_bookings():
 
     pass
 
+#----------------------------------------------------------
 # Function 5
+#----------------------------------------------------------
 def save_bookings():
     # TODO -- save the entries as a csv (comma seperated file) in the current folder called bookings.csv
     pass
 
+#----------------------------------------------------------
 # Function 6
+#----------------------------------------------------------
 def read_bookings():
     # TODO -- check if the file exists
 
@@ -167,12 +208,12 @@ def read_bookings():
     pass    
 
 
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # Main
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 print("Welcome to the Nemo Reef Tours Management System")
 process_menu_item() 
 
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 # End
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
